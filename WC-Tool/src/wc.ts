21de8d80be101fc2a8bac789622cdcd776defbab
sodia <gauravsodia@gmail.com>
2024-08-30 22:08:27 +0530

    wc fixes

--- WC-Tool/src/wc.ts
+++ WC-Tool/src/wc.ts
@@ -1,158 +1,122 @@
-import fs from 'fs';
+import * as fs from "fs";
+import * as path from "path";
 
-/**
- * This function Reads the given stream and returns a Buffer.
- *
- * @async
- * @param {NodeJS.ReadStream} stream
- * @returns {Promise<Buffer>}
- */
-async function readStream(
-  stream: NodeJS.ReadStream | fs.ReadStream
-): Promise<Buffer> {
-  const chunks : any = [];
-  for await (const chunk of stream) {
-    chunks.push(Buffer.from(chunk));
-  }
-  return Buffer.concat(chunks);
-}
+export default class Wc{
 
-/**
- * This function returns the number of bytes consisting the given file.
- *
- * @param {string} filename
- * @returns {number}
- */
-function byteCount(filename: string): number {
-  return fs.statSync(filename).size;
-}
-
-/**
- * This function returns the number of lines in a given text.
- *
- * @param {string} text
- * @returns {number}
- */
-function lineCount(text: string): number {
-  return text.split(/\r\n|\r|\n/).length - 1;
-}
-
-/**
- * This function returns the number of words stored in a text.
- *
- * @param {string} text
- * @returns {number}
- */
-function wordCount(text: string): number {
-  if (text.length <= 0) {
-    return 0;
-  }
-  return text.trim().split(/\s+/).length;
-}
-
-/**
- * This function returns the number of characters in a given text.
- *
- * @param {string} text
- * @returns {number}
- */
-function charCount(text: string): number {
-  return text.length;
-}
-
-/**
- * This function is the unix wc command implementation
- *
- * @async
- * @param {string[]} argv - The first two are reserved arguments considering the
- *      node call to the file
- * @param {?NodeJS.ReadStream | fs.ReadStream } [stream] - This can be a file read stream or the
- *      stdin stream
- * @returns {Promise<string>}
- */
-async function myWC(
-  argv: string[],
-  stream?: NodeJS.ReadStream | fs.ReadStream
-): Promise<string> {
-  // Option is given, file is given
-  if (argv.length === 4) {
-    // console.log('file + option');
-    const option = argv[2];
-    const filename = argv[3];
-    if (fs.existsSync(filename)) {
-      const fileContents = fs.readFileSync(filename, 'utf8').toString();
-      switch (option) {
-        case '-c':
-          return byteCount(filename).toString() + ' ' + filename;
-        case '-l':
-          return lineCount(fileContents).toString() + ' ' + filename;
-        case '-w':
-          return wordCount(fileContents).toString() + ' ' + filename;
-        case '-m':
-          return charCount(fileContents) + ' ' + filename;
-        default:
-          throw new Error('Invalid option');
-      }
+  /**
+   * this function read a given stream and returns a promise of string
+   * @async
+   * @param {NodeJS.ReadStream} stream
+   * @returns {Promise<string>}
+   */
+  static async streamToString(
+    stream: NodeJS.ReadStream | fs.ReadStream
+  ) : Promise<string> {
+    const chunks: any = [];
+    for await(const chunk of stream){
+      chunks.push(Buffer.from(chunk));
     }
-    if (typeof stream === 'undefined') {
-      throw new Error('Invalid file');
+
+    return Buffer.concat(chunks).toString("utf-8");
+  }
+
+  /**
+   * this function return the byte of given string
+   * @param {string} text
+   * @returns {number}
+   */
+  static getBytesCount(text: string): number{
+    return new Blob([text]).size;
+  }
+
+  /**
+   * this function return the count of lines
+   * 
+   * @param {string} text
+   * @returns {number}
+   */
+  static getLinesCount(text: string): number {
+    return text.split(/\r\n|\r|\n/).length;
+  }
+
+  /**
+   * this return words in a file
+   * @param { string } text 
+   * @returns {number}
+   */
+  static getWordCount(text:string): number {
+    if(text.length<=0) return 0;
+
+    return text.trim().split(/\s+/).length
+  }
+
+  /**
+   * this return count of char in a file
+   * @param {string} text 
+   * @returns {number}
+   */
+  static getCharCount(text: string): number {
+    return text.length;
+  }
+
+  /**
+   * 
+   * @param option 
+   * @param text 
+   * @returns 
+   */
+  static handleOptions(option: string, text: string): string {
+    switch (option.toLowerCase()){
+      case "-c": 
+        return `${this.getBytesCount(text)}`;
+      case "-l":
+        return `${this.getLinesCount(text)}`;
+      case "-w":
+        return `${this.getWordCount(text)}`;
+      case "-m":
+        return `${this.getCharCount(text)}`;
+      case "":
+        return `${this.getBytesCount(text)} ${this.getLinesCount(text)} ${this.getWordCount(text)} ${this.getCharCount(text)}`;
+      default:
+        throw new Error("Can't find option");
     }
   }
 
-  // Only filename is given
-  if (argv.length === 3) {
-    // console.log('only option');
-    const filename = argv[2];
-    if (fs.existsSync(filename)) {
-      const fileContents = fs.readFileSync(filename, 'utf8');
-      const line = lineCount(fileContents).toString();
-      const word = wordCount(fileContents).toString();
-      const bytes = byteCount(filename).toString();
-      return line + ' ' + word + ' ' + bytes + ' ' + filename;
-    }
-    if (typeof stream === 'undefined') {
-      throw new Error('Invalid file');
-    }
+  static async countFromStream(stream: NodeJS.ReadStream | fs.ReadStream, option: string): Promise<string>{
+    const text = await this.streamToString(stream);
+    return this.handleOptions(option,text);
+  } 
+
+  /**
+   * 
+   * @param file 
+   * @param option 
+   * @returns 
+   */
+  static countFromFile(file: string, option: string): string {
+    const filePath = path.join(__dirname, file);
+    const text = fs.readFileSync(filePath, "utf8");
+    return this.handleOptions(option, text);
   }
 
-  // Checking for stream
-  if (typeof stream !== 'undefined') {
-    // console.log('stream');
-    try {
-      // If option is given
-      const buffer = await readStream(stream);
-      const fileContents = buffer.toString();
-      if (argv.length === 3) {
-        const option = argv[2];
-        switch (option) {
-          case '-c':
-            return buffer.length.toString();
-          case '-l':
-            return lineCount(fileContents).toString();
-          case '-w':
-            return wordCount(fileContents).toString();
-          case '-m':
-            return charCount(fileContents).toString();
-          default:
-            throw new Error('Invalid option');
-        }
-      }
-
-      // If no option is given
-      if (argv.length == 2) {
-        const line = lineCount(fileContents).toString();
-        const word = wordCount(fileContents).toString();
-        const bytes = buffer.length.toString();
-        return line + ' ' + word + ' ' + bytes;
-      }
-    } catch (err) {
-      // If the error is not an expected TypeError
-      if (!(err instanceof TypeError)) {
-        throw err;
-      }
+/**
+ * 
+ * @param param0 
+ * @returns 
+ */
+  static async myWC({option, file, stream}: {
+    option: string, file? : string, stream? : NodeJS.ReadStream | fs.ReadStream
+  }): Promise<string>{
+    let text;
+    if(stream){
+      text = await this.countFromStream(stream, option);
+    }else if(file){
+      text = await this.countFromFile(file, option);
+    }else{
+      throw new Error(`Provide file or stream`);
     }
+
+    return text;
   }
-  throw new Error('Invalid input or file');
+
 }
-
-export { myWC };
